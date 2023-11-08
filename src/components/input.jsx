dd995142032debba4bd07dff6924f188477ceaa4
Sreesanth <sreesanth46@gmail.com>
2023-11-09 00:52:04 +0530

    tests: user events

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 const AppInput = () => {
+  const [value, setValue] = useState('');
+
+  const handleInput = e => {
+    setValue(e.target.value)
+  };
+
   return (
     <>
-      <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white;">Name</label>
-      <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+      <label
+        htmlFor="name"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white;">
+        Name
+      </label>
+      <input
+        type="text"
+        value={value}
+        onChange={handleInput}
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
     </>
   );
 };
 
-export default AppInput
+export default AppInput;
--- src/components/input.spec.js
+++ src/components/input.spec.js
@@ -1,13 +1,27 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor} from "@testing-library/react"
 import AppInput from "./input"
+import userEvent from "@testing-library/user-event"
+import { act } from "react-dom/test-utils"
 
-describe("Input component", () =>{
+describe("Input component", () => {
     it("should render", () => {
         render(
-            <AppInput/>
+            <AppInput />
         )
         const input = screen.getByRole('textbox')
         expect(input).toBeInTheDocument()
+    })
+
+    it("should be able to type texts", async () => {
+        await act(async() => {
+            render(<AppInput />)
+        })
+        const input = screen.getByRole('textbox')
+
+        userEvent.type(input, "Hello")
+
+        expect(input).toHaveValue('Hello')
+
     })
 
 })
