779accc43660a1c09d5f9e09ec9e93001960f4f1
Sreesanth <sreesanth46@gmail.com>
2023-11-10 00:36:16 +0530

    tests: render

---
+++ src/components/AppInput.spec.js
@@ -0,0 +1,46 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import AppInput from "./AppInput"
+import userEvent from "@testing-library/user-event"
+import { act } from "react-dom/test-utils"
+import { useState } from "react"
+
+describe("Input component", () => {
+    let Component
+    function handleChange(e) {
+        const { value } = e.target
+        setValue = value
+    }
+
+
+    beforeEach(async () => {
+        await act(async () => {
+            Component = render(
+                <AppInput
+                    label={"name"}
+                    onChange={handleChange}
+                />
+            )
+        })
+    })
+
+    it("should render", () => {
+        const input = Component.getByRole('textbox')
+        expect(input).toBeInTheDocument()
+    })
+
+    it("should be able to type texts", async () => {
+
+        const input = Component.getByRole('textbox')
+
+        await waitFor(() => {
+            userEvent.type(input, "Hello")
+        }) 
+            
+        
+        screen.debug(input)
+
+        expect(input).toHaveValue('Hello')
+
+    })
+
+})
